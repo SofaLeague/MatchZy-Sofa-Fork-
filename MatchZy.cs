a649aaab63591ef4df1a326cd21cc82a70acc58f
b3spredel1337 <problema22801@gmail.com>
2025-06-03 20:50:56 +0300

    monsterenergy1

--- MatchZy.cs
+++ MatchZy.cs
@@ -15,7 +15,7 @@ namespace MatchZy
     {
 
         public override string ModuleName => "MatchZy [Sofa Fork]";
-        public override string ModuleVersion => "0.8.10";
+        public override string ModuleVersion => "0.8.11";
 
         public override string ModuleAuthor => "WD- (https://github.com/shobhit-pathak/)";
 
